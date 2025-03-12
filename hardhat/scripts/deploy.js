86958d2d06c7876f133ee4d276e5b7960cd4c296
236sec <popole159za@gmail.com>
2025-03-12 18:25:06 +0700

    Deploy NFTMarketplace

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+  const marketplace = await NFTMarketplace.deploy();
+  console.log("NFTMarketplace deployed to:", marketplace.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
